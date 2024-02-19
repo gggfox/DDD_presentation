0b1be66438acf4db52f66dc67d193a0897ce8525
gggfox <gerardogalangarzafox@gmail.com>
2024-02-19 08:32:02 -0600

    first commit

---
+++ src/lib/deck/presentation/StrategicDesign.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+	import Slide from './../slide.svelte'
+	import Code from './../code.svelte'
+	import Markdown from './../markdown.svelte'
+</script>
+<Slide>
+	<Slide>
+		<p class="capitalize">Strategic Design 🧭</p>
+	</Slide>
+	<Slide>
+		<p class="capitalize">Whats a business domain?</p>
+	</Slide>
+    <Slide animate>
+        <div class="flex gap-2 place-content-center">
+            <div class="border-4 border-lime-300 w-[33%]">
+                <p class="capitalize text-3xl">Core Subdomain</p>
+                <p class="text-2xl">
+                    It's what distinguishes the business from the rest,
+                    giving it a competitive advantage
+                </p>
+            </div>
+        </div>
+	</Slide>
+    <Slide animate>
+        <div class="flex gap-2 place-content-center">
+            <div class="border-4 border-lime-300 w-[33%]">
+
+                <p class="capitalize text-3xl">
+                    Core Subdomain
+                </p>
+                <p class="text-2xl">
+                    It's what distinguishes the business from the rest,
+                    giving it a competitive advantage
+                </p>
+            </div>
+            <div class="border-4 border-lime-300 text-3xl w-[33%]">
+                <p class="capitalize">Generic Subdomain</p>
+                <p class="text-2xl">
+                    It's what distinguishes the business from the rest,
+                    giving it a competitive advantage
+                </p>
+            </div>
+        </div>
+	</Slide>
+    <Slide animate>
+        <div class="flex gap-2 place-content-center">
+            <div class="border-4 border-lime-300 w-[33%]">
+                <p class="capitalize text-3xl">Core Subdomain</p>
+                <p class="text-2xl">
+                    It's what distinguishes the business from the rest,
+                    giving it a competitive advantage
+                </p>
+            </div>
+            <div class="border-4 border-lime-300 text-3xl w-[33%]">
+                <p class="capitalize">Generic Subdomain</p>
+                <p class="text-2xl">
+                    It's what distinguishes the business from the rest,
+                    giving it a competitive advantage
+                </p>
+            </div>
+            <div class="border-4 border-lime-300 text-3xl w-[33%]">
+                <p class="capitalize">Support Subdomain</p>
+                <p class="text-2xl">
+                    It's what distinguishes the business from the rest,
+                    giving it a competitive advantage
+                </p>
+            </div>
+        </div>
+	</Slide>
+</Slide>
